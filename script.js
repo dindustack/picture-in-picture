f95d3ce0e9e7aff18e3cc7eba3c585cb21b40f5c
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2021-02-18 23:01:40 +0100

    final commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.onplay();
+        }
+    } catch (error) {
+        
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // reset button 
+    button.disabled = false;
+
+})
+
+// On Load
+selectMediaStream(); 
